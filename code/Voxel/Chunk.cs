f4cc7063e26c693ebadc9b1c2d46aa2a5d2b69f7
ceitine <ceitinee@gmail.com>
2023-07-05 20:14:45 +0300

    add all directions to chunking, first implementation of turning world pos to voxel position and chunk position

--- code/Voxel/Chunk.cs
+++ code/Voxel/Chunk.cs
@@ -4,29 +4,31 @@ public class Chunk
 {
 	public const ushort DEFAULT_WIDTH = 16;
 	public const ushort DEFAULT_DEPTH = 16;
-	public const ushort DEFAULT_HEIGHT = 32;
+	public const ushort DEFAULT_HEIGHT = 16;
 
 	private VoxelEntity parent;
 	private Voxel?[,,] voxels;
 
 	public ushort X;
 	public ushort Y;
+	public ushort Z;
 
 	public ushort Width => (ushort)voxels.GetLength( 0 );
 	public ushort Depth => (ushort)voxels.GetLength( 1 );
 	public ushort Height => (ushort)voxels.GetLength( 2 );
 
-	public Chunk( ushort x, ushort y, ushort width = DEFAULT_WIDTH, ushort depth = DEFAULT_DEPTH, ushort height = DEFAULT_HEIGHT, VoxelEntity entity = null )
+	public Chunk( ushort x, ushort y, ushort z, ushort width = DEFAULT_WIDTH, ushort depth = DEFAULT_DEPTH, ushort height = DEFAULT_HEIGHT, VoxelEntity entity = null )
 	{
 		this.X = x;
 		this.Y = y;
+		this.Z = z;
 
 		parent = entity;
 		voxels = new Voxel?[width, depth, height];
 
 		for ( x = 0; x < width; x++ )
 		for ( y = 0; y < depth; y++ )
-		for ( ushort z = 0; z < height; z++ )
+		for ( z = 0; z < height; z++ )
 		{
 			SetVoxel( x, y, z, new Voxel( Color.Random.ToColor32() ), true );
 		}
@@ -36,7 +38,12 @@ public class Chunk
 	}
 
 	public Voxel? GetVoxel( ushort x, ushort y, ushort z )
-		=> voxels[x, y, z];
+	{
+		if ( x < 0 || y < 0 || z < 0
+		  || x >= Width || y >= Depth || z >= Height ) return null;
+
+		return voxels[x, y, z];
+	}
 
 	public void SetVoxel( ushort x, ushort y, ushort z, Voxel? voxel = null, bool generating = false )
 	{
--- code/Voxel/VoxelEntity.cs
+++ code/Voxel/VoxelEntity.cs
@@ -1,20 +1,31 @@
 ﻿namespace DeathCard;
 
+public struct VoxelData
+{
+	public Voxel Voxel;
+	public Chunk Chunk;
+
+	public ushort X;
+	public ushort Y;
+	public ushort Z;
+}
+
 public partial class VoxelEntity : ModelEntity
 {
 	public float VoxelScale { get; set; } = 1f / 0.0254f;
-	public Chunk[,] Chunks;
+	public Chunk[,,] Chunks;
 	public Dictionary<Chunk, ModelEntity> Entities = new();
 
 	public VoxelEntity( Vector3? position = null )
 	{
 		Position = position ?? Vector3.Zero;
 
-		Chunks = new Chunk[2, 2];
+		Chunks = new Chunk[2, 2, 2];
 
-		for ( ushort x = 0; x < 2; x++ )
-		for ( ushort y = 0; y < 2; y++ )
-			Chunks[x, y] = new( x, y, entity: this );
+		for ( ushort x = 0; x < Chunks.GetLength( 0 ); x++ )
+		for ( ushort y = 0; y < Chunks.GetLength( 1 ); y++ )
+		for ( ushort z = 0; z < Chunks.GetLength( 2 ); z++ )
+			Chunks[x, y, z] = new( x, y, z, entity: this );
 	}
 
 	public void OnChunkChanged( Chunk chunk, ushort x, ushort y, ushort z )
@@ -88,10 +99,14 @@ public partial class VoxelEntity : ModelEntity
 
 			var faces = 6;
 			var shouldHide = new bool[faces];
-			/*var neighbors = new (short x, short y, short z)[]
+			var neighbors = new (short x, short y, short z)[]
 			{
 				(0, 0, 1),
 				(0, 0, -1),
+				(-1, 0, 0),
+				(0, 1, 0),
+				(1, 0, 0),
+				(0, -1, 0),
 			};
 
 			for( int i = 0; i < neighbors.Length; i++ )
@@ -103,7 +118,7 @@ public partial class VoxelEntity : ModelEntity
 
 				var neighbor = chunk.GetVoxel( (ushort)(x + direction.x), (ushort)(y + direction.y), (ushort)(z + direction.z) );
 				shouldHide[i] = neighbor != null;
-			}*/
+			}
 			
 			var drawCount = 0;
 			for ( var i = 0; i < faces; ++i )
@@ -124,12 +139,12 @@ public partial class VoxelEntity : ModelEntity
 					vertices.Add( new VoxelVertex( pos, normal, voxel.Value.Color ) );
 				}
 
-				indices.Add( offset + i * 4 + 0 );
-				indices.Add( offset + i * 4 + 2 );
-				indices.Add( offset + i * 4 + 1 );
-				indices.Add( offset + i * 4 + 2 );
-				indices.Add( offset + i * 4 + 0 );
-				indices.Add( offset + i * 4 + 3 );
+				indices.Add( offset + drawCount * 4 + 0 );
+				indices.Add( offset + drawCount * 4 + 2 );
+				indices.Add( offset + drawCount * 4 + 1 );
+				indices.Add( offset + drawCount * 4 + 2 );
+				indices.Add( offset + drawCount * 4 + 0 );
+				indices.Add( offset + drawCount * 4 + 3 );
 
 				drawCount++;
 			}
@@ -137,17 +152,83 @@ public partial class VoxelEntity : ModelEntity
 			offset += 4 * drawCount;
 		}
 
+		var ind = indices.ToArray();
 		mesh.CreateVertexBuffer<VoxelVertex>( vertices.Count, VoxelVertex.Layout, vertices.ToArray() );
-		mesh.CreateIndexBuffer( indices.Count, indices.ToArray() );
+		mesh.CreateIndexBuffer( indices.Count, ind );
 
 		// Create a model for the mesh.
 		var model = Model.Builder
 			.AddMesh( mesh )
+			.AddCollisionMesh( vertices.Select( v => v.position ).ToArray(), ind )
 			.Create();
 
 		chunkEntity.Model = model;
-		chunkEntity.Position = Position + new Vector3( chunk.X * chunk.Width, chunk.Y * chunk.Depth ) * VoxelScale;
+		chunkEntity.Position = Position + new Vector3( chunk.X * chunk.Width, chunk.Y * chunk.Depth, chunk.Z * chunk.Height ) * VoxelScale + VoxelScale / 2f;
 		chunkEntity.SetupPhysicsFromModel( PhysicsMotionType.Static );
+	}
+
+	public VoxelData? GetClosestVoxel( Vector3 position, ushort x = Chunk.DEFAULT_WIDTH, ushort y = Chunk.DEFAULT_DEPTH, ushort z = Chunk.DEFAULT_HEIGHT )
+	{
+		var relative = position - Position;
+		var chunkPosition = new Vector3( relative.x / (x * VoxelScale), relative.y / (y * VoxelScale), relative.z / (z * VoxelScale) );
+		var w = Chunks.GetLength( 0 );
+		var d = Chunks.GetLength( 1 );
+		var h = Chunks.GetLength( 2 );
+
+		var voxelIndex = (
+			x: (ushort)MathF.Abs( chunkPosition.x * x ).FloorToInt().Clamp( 0, x * w ), 
+			y: (ushort)MathF.Abs( chunkPosition.y * y ).FloorToInt().Clamp( 0, y * d ), 
+			z: (ushort)MathF.Abs( chunkPosition.z * z ).FloorToInt().Clamp( 0, z * h )
+		);
+		var chunkIndex = (
+			x: (voxelIndex.x / x).Clamp( 0, w - 1 ),
+			y: (voxelIndex.y / y).Clamp( 0, d - 1 ),
+			z: (voxelIndex.z / z).Clamp( 0, h - 1 )
+		);
+		
+		var chunk = Chunks[chunkIndex.x, chunkIndex.y, chunkIndex.z];
+		var voxel = (
+			x: (ushort)(voxelIndex.x - chunkIndex.x * x).Clamp( 0, x - 1 ),
+			y: (ushort)(voxelIndex.y - chunkIndex.y * y).Clamp( 0, y - 1 ),
+			z: (ushort)(voxelIndex.z - chunkIndex.z * z).Clamp( 0, z - 1 )
+		);
+
+		return new VoxelData
+		{
+			Chunk = chunk,
+			Voxel = chunk.GetVoxel( voxel.x, voxel.y, voxel.z ) ?? default,
+			X = voxel.x,
+			Y = voxel.y,
+			Z = voxel.z
+		};
+	}
+
+	// TODO:
+	public Voxel? GetNeighbor( Chunk chunk, Voxel voxel, ushort x, ushort y, ushort z )
+	{
+		return null;
+	}
+
+	[GameEvent.Client.Frame]
+	private void tick()
+	{
+		if ( Game.LocalPawn is not Pawn pawn )
+			return;
+
+		var ray = new Ray( pawn.Position, pawn.ViewAngles.Forward );
+		var tr = Trace.Ray( ray, 10000f )
+			.StaticOnly()	
+			.IncludeClientside()
+			.Run();
+
+		DebugOverlay.TraceResult( tr );
+
+		var voxelData = ent?.GetClosestVoxel( tr.EndPosition - tr.Normal * VoxelScale / 2f );
+		if ( voxelData != null && Input.Down( "attack2" ) )
+		{
+			var data = voxelData.Value;
+			data.Chunk.SetVoxel( data.X, data.Y, data.Z, new Voxel( Color.Black ) );
+		}
 	}
 
 	static VoxelEntity ent;
