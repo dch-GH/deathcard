31c691ef255a1e28958536e3a6498a8e97f5e642
ceitine <ceitinee@gmail.com>
2023-08-09 00:17:02 +0300

    add testing binds to pawn, m1 to destroy, e to grenade

--- code/Game.cs
+++ code/Game.cs
@@ -5,6 +5,7 @@ global using System.IO;
 global using System.Linq;
 global using System.Threading.Tasks;
 global using System.Collections.Generic;
+global using System.Collections.ObjectModel;
 global using System.Text;
 global using System.Runtime.InteropServices;
 
--- code/Pawn.cs
+++ code/Pawn.cs
@@ -61,6 +61,57 @@ partial class Pawn : AnimatedEntity
 		{
 			Position = helper.Position;
 		}
+
+		// Throw Grenade by pressing E.
+		if ( Input.Pressed( "use" ) && Game.IsServer )
+		{
+			var force = 2000f;
+			_ = new Bomb()
+			{
+				Size = Game.Random.Int( 2, 12 ),
+				Delay = Game.Random.Float( 1, 5 ),
+				Position = Position + ViewAngles.Forward * 50f,
+				Velocity = force * ViewAngles.Forward
+			};
+		}
+
+		// Destruction
+		var ray = new Ray( Position, ViewAngles.Forward );
+		var tr = Trace.Ray( ray, 10000f )
+			.IncludeClientside()
+			.WithTag( "chunk" )
+			.Run();			
+
+		var parent = (tr.Entity as ChunkEntity)?.Parent;
+		var position = parent?.WorldToVoxel( tr.EndPosition - tr.Normal * parent.VoxelScale / 2f );
+		if ( position == null )
+			return;
+
+		var value = position.Value;
+		var chunk = (Chunk)null;
+		var pos = parent?.GetLocalSpace( value.x, value.y, value.z, out chunk );
+
+		if ( Input.Pressed( "attack1" ) )
+		{
+			var size = 8;
+			for ( int x = 0; x < size; x++ )
+			for ( int y = 0; y < size; y++ )
+			for ( int z = 0; z < size; z++ )
+			{
+				var center = (Vector3)pos;
+				var target = center
+					+ new Vector3( x, y, z )
+					- size / 2f;
+
+				if ( target.Distance( center ) >= size / 2f )
+					continue;
+
+				parent.SetVoxel(
+					target.x.FloorToInt(),
+					target.y.FloorToInt(),
+					target.z.FloorToInt(), null, chunk );
+			}
+		}
 	}
 
 	/// <summary>
