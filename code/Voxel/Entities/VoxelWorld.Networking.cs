452e395c375b370b6e3a33c0796320c848b0ab9b
ceitine <ceitinee@gmail.com>
2023-08-09 00:16:04 +0300

    initial state of voxel networking

---
+++ code/Voxel/Entities/VoxelWorld.Networking.cs
@@ -0,0 +1,257 @@
+﻿namespace DeathCard;
+
+partial class VoxelWorld
+{
+	#region Fields
+	private static VoxelWorld instance;
+	private Dictionary<Vector3I, Change> changes = new();
+	#endregion
+
+	/// <summary>
+	/// Initializes a VoxelWorld, should be called on server.
+	/// </summary>
+	/// <returns></returns>
+	public static async Task<VoxelWorld> Create( string map = null, Vector3I? chunkSize = null )
+	{
+		// Create a VoxelWorld and load a map for it.
+		var world = new VoxelWorld()
+		{
+			ChunkSize = chunkSize ?? new( Chunk.DEFAULT_WIDTH, Chunk.DEFAULT_DEPTH, Chunk.DEFAULT_HEIGHT ),
+		};
+
+		var chunks = await Importer.VoxImporter.Load( map, world.ChunkSize.x, world.ChunkSize.y, world.ChunkSize.z );
+		world.Size = new Vector3I( chunks.GetLength( 0 ), chunks.GetLength( 0 ), chunks.GetLength( 0 ) );
+		world.Chunks = chunks;
+
+		// Initial chunk generation.
+		foreach ( var chunk in world.Chunks )
+			world.GenerateChunk( chunk );
+
+		// Send the map to all clients.
+		world.LoadAsMap( To.Everyone, world.NetworkIdent, map ?? string.Empty );
+
+		return world;
+	}
+
+	[ClientRpc]
+	public async void LoadAsMap( int ident, string map )
+	{
+		// Make sure we have a valid VoxelWorld.
+		if ( Entity.FindByIndex( ident ) is not VoxelWorld entity )
+		{
+			Log.Error( $"Failed to find VoxelWorld[{ident}]." );
+			return;
+		}
+
+		// Create same map as server.
+		var chunks = await Importer.VoxImporter.Load( map, entity.ChunkSize.x, entity.ChunkSize.y, entity.ChunkSize.z );
+		entity.Chunks = chunks;
+
+		// TODO: Apply server's changes here.
+		// Maybe multithread?
+
+		// Initial chunk generation.
+		foreach ( var chunk in entity.Chunks )
+			entity.GenerateChunk( chunk );
+	}
+
+	[ConCmd.Server( "loadmap" )]
+	public static async void LoadMap( string map = "vox/monument.vox" )
+	{
+		instance?.Delete();
+		instance = await VoxelWorld.Create( "vox/monument.vox" );
+	}
+
+	#region Networking
+	public enum VoxelState
+	{
+		Invalid,
+		Valid
+	}
+
+	public struct Change
+	{
+		public VoxelState State;
+		public Voxel? Voxel;
+		public Voxel? Previous;
+	}
+
+	private bool RegisterChange( Chunk chunk, Vector3I pos, Voxel? voxel )
+	{
+		// Check if we are within chunk bounds.
+		if ( pos.x > ChunkSize.x - 1 || pos.y > ChunkSize.y - 1 || pos.z > ChunkSize.z - 1
+		  || pos.x < 0 || pos.y < 0 || pos.z < 0 ) return false;
+
+		var previous = chunk.GetVoxel( pos.x, pos.y, pos.z );
+		if ( previous == null && voxel == null )
+			return false;
+
+		var position = GetGlobalSpace( pos.x, pos.y, pos.z, chunk );
+		if ( changes.TryGetValue( position, out var change ) )
+		{
+			change.Previous = previous;
+			change.Voxel = voxel;
+			change.State = voxel == null
+				? VoxelState.Invalid
+				: VoxelState.Valid;
+
+			return true;
+		}
+
+		changes.Add( position, new Change()
+		{
+			Voxel = voxel,
+			Previous = previous,
+			State = voxel == null
+				? VoxelState.Invalid
+				: VoxelState.Valid
+		} );
+
+		return true;
+	}
+
+	/// <summary>
+	/// Sets voxel relative to a chunk, coordinates cannot be greater or equal to ChunkSize or less than 0.
+	/// </summary>
+	/// <param name="chunk"></param>
+	/// <param name="x"></param>
+	/// <param name="y"></param>
+	/// <param name="z"></param>
+	/// <param name="voxel"></param>
+	public void SetVoxel( Chunk chunk, ushort x, ushort y, ushort z, Voxel? voxel )
+	{
+		if ( !RegisterChange( chunk, new( x, y, z ), voxel ) )
+			return;
+
+		chunk.SetVoxel( x, y, z, voxel );
+	}
+
+	/// <summary>
+	/// Sets voxel by offset, relative to the chunk parameter or Chunks[0, 0, 0].
+	/// </summary>
+	/// <param name="x"></param>
+	/// <param name="y"></param>
+	/// <param name="z"></param>
+	/// <param name="voxel"></param>
+	/// <param name="relative"></param>
+	public void SetVoxel( int x, int y, int z, Voxel? voxel, Chunk relative = null )
+	{
+		// Convert to local space.
+		var pos = GetLocalSpace( x, y, z, out var chunk, relative );
+		if ( chunk == null )
+			return;
+
+		// Set voxel.
+		SetVoxel( chunk, pos.x, pos.y, pos.z, voxel );
+	}
+
+	private void Update()
+	{
+		// Initialize our MemoryStream.
+		using var stream = new MemoryStream();
+		using var writer = new BinaryWriter( stream );
+
+		// Let's keep track of all the chunks that we need to update.
+		var chunks = new Collection<Chunk>();
+
+		// Start writing data.
+		var count = changes.Count;
+		writer.Write( count );
+
+		foreach ( var (position, change) in changes )
+		{
+			writer.Write( (byte)change.State );
+			writer.Write( position.x );
+			writer.Write( position.y );
+			writer.Write( position.z );
+
+			var pos = GetLocalSpace( position.x, position.y, position.z, out var chunk );
+			var neighbors = chunk.GetNeighbors( pos.x, pos.y, pos.z );
+			foreach ( var neighbor in neighbors )
+				if ( neighbor != null && !chunks.Contains( neighbor ) )
+					chunks.Add( neighbor );
+
+			if ( change.State == VoxelState.Invalid )
+				continue;
+
+			var voxel = change.Voxel.Value;
+			writer.Write( voxel.R );
+			writer.Write( voxel.G );
+			writer.Write( voxel.B );
+		}
+
+		// Rebuild all affected chunks.
+		foreach ( var chunk in chunks )
+			GenerateChunk( chunk );
+
+		// Send update to all clients.
+		SendUpdate( To.Everyone, stream.ToArray() );
+	}
+
+	/// <summary>
+	/// Sends an update of voxel changes to a client.
+	/// </summary>
+	[ClientRpc]
+	public void SendUpdate( byte[] data )
+	{
+		// Initialize our MemoryStream.
+		using var stream = new MemoryStream( data );
+		using var reader = new BinaryReader( stream );
+
+		// Let's keep track of all the chunks that we need to update.
+		var chunks = new Collection<Chunk>();
+
+		// Start reading data.
+		var count = reader.ReadInt32();
+		for ( int i = 0; i < count; i++ )
+		{
+			var state = (VoxelState)reader.ReadByte();
+			var position = new Vector3I( reader.ReadUInt16(), reader.ReadUInt16(), reader.ReadUInt16() );
+
+			var voxel = state == VoxelState.Invalid
+				? (Voxel?)null
+				: new Voxel( new Color32( reader.ReadByte(), reader.ReadByte(), reader.ReadByte() ) );
+
+			var pos = GetLocalSpace( position.x, position.y, position.z, out var chunk );
+			chunk.SetVoxel( pos.x, pos.y, pos.z, voxel );
+
+			var neighbors = chunk.GetNeighbors( pos.x, pos.y, pos.z );
+			foreach ( var neighbor in neighbors )
+				if ( neighbor != null && !chunks.Contains( neighbor ) )
+					chunks.Add( neighbor );
+
+			// Check if our Voxel matches on client & server.
+			if ( changes.TryGetValue( position, out var change ) && change.State == state && change.Voxel?.Color == voxel?.Color )
+				changes.Remove( position );
+		}
+
+		// TODO: Go through all client changes, revert.
+		// This doesn't work properly yet.
+		/*foreach ( var (pos, change) in changes )
+		{
+			var position = GetLocalSpace( pos.x, pos.y, pos.z, out var chunk );
+			if ( chunk == null )
+				continue;
+			Log.Error( $"Voxel mismatch at {pos}!" );
+			chunk.SetVoxel( position.x, position.y, position.z, change.Previous );
+		}*/
+
+		changes.Clear();
+
+		// Update changed chunks.
+		foreach ( var chunk in chunks )
+			GenerateChunk( chunk );
+	}
+
+	[GameEvent.Tick]
+	private void Tick()
+	{
+		// Let's apply existing changes on server and network them.
+		if ( Game.IsServer && changes.Count > 0 )
+		{
+			Update();
+			changes.Clear();
+		}
+	}
+	#endregion
+}
--- code/Voxel/Entities/VoxelWorld.cs
+++ code/Voxel/Entities/VoxelWorld.cs
@@ -12,14 +12,6 @@ if ( voxel != null )
 	particle.SetPosition( 2, col );
 }
 */
-
-public struct VoxelData
-{
-	public Voxel? Voxel;
-	public Chunk Chunk;
-	public Vector3I Position;
-}
-
 public class ChunkEntity : ModelEntity
 {
 	public new VoxelWorld Parent { get; set; }
@@ -38,25 +30,32 @@ public partial class VoxelWorld : ModelEntity
 
 	public Chunk[,,] Chunks { get; private set; }
 
-	public Vector3I Size { get; private set; }
-	public Vector3I ChunkSize { get; private set; }
+	[Net] public Vector3I Size { get; private set; }
+	[Net] public Vector3I ChunkSize { get; private set; }
 
 	public VoxelWorld() 
 	{
 		ChunkSize = new( Chunk.DEFAULT_WIDTH, Chunk.DEFAULT_DEPTH, Chunk.DEFAULT_HEIGHT );
 	}
 
+	protected override void OnDestroy()
+	{
+		foreach ( var child in entities.Values )
+			child.Delete();
+
+		Chunks = null;
+	}
+
 	/// <summary>
 	/// Generates a chunk.
 	/// </summary>
 	/// <param name="chunk"></param>
-	/// <param name="withPhysics"></param>
-	public void GenerateChunk( Chunk chunk, bool withPhysics = true )
+	public void GenerateChunk( Chunk chunk )
 	{
 		// Get our chunk's entity.
 		if ( chunk == null )
 			return;
-
+		
 		ModelEntity chunkEntity;
 		if ( !entities.TryGetValue( chunk, out chunkEntity ) )
 			entities.Add( chunk, chunkEntity = new ChunkEntity() { Parent = this } );
@@ -85,7 +84,7 @@ public partial class VoxelWorld : ModelEntity
 			var position = new Vector3I( x, y, z );
 
 			// Let's start checking for collisions.
-			if ( withPhysics && !tested[x, y, z] )
+			if ( !tested[x, y, z] )
 			{
 				tested[x, y, z] = true;
 
@@ -152,219 +151,21 @@ public partial class VoxelWorld : ModelEntity
 		mesh.CreateIndexBuffer( indices.Count, indices.ToArray() );
 
 		// Create a model for the mesh.
-		builder.AddMesh( mesh );
+		builder.AddCollisionMesh( buffer.Vertex.ToArray(), buffer.Index.ToArray() );
 
-		// Build collisions.
-		if ( withPhysics )
-			builder.AddCollisionMesh( buffer.Vertex.ToArray(), buffer.Index.ToArray() );
+		if ( Game.IsClient )
+			builder.AddMesh( mesh );
 
 		chunkEntity.Model = builder.Create();
 		chunkEntity.Position = Position
 			+ (Vector3)chunk.Position * ChunkSize * VoxelScale
 			+ VoxelScale / 2f;
 
-		if ( withPhysics )
-			chunkEntity.SetupPhysicsFromModel( PhysicsMotionType.Static );
-	}
-
-	/// <summary>
-	/// Attempts to get VoxelData from an absolute world position.
-	/// </summary>
-	/// <param name="position"></param>
-	/// <returns></returns>
-	public VoxelData? GetClosestVoxel( Vector3 position )
-	{
-		var width = ChunkSize.x;
-		var depth = ChunkSize.y;
-		var height = ChunkSize.z;
-		
-		var relative = position - Position;
-		var chunkPosition = new Vector3( relative.x / (width * VoxelScale), relative.y / (depth * VoxelScale), relative.z / (height * VoxelScale) );
-
-		if ( chunkPosition.x < 0 || chunkPosition.y < 0 || chunkPosition.z < 0 )
-			return null;
-
-		var voxelIndex = (
-			x: (short)(chunkPosition.x * width).FloorToInt(), 
-			y: (short)(chunkPosition.y * depth).FloorToInt(), 
-			z: (short)(chunkPosition.z * height).FloorToInt()
-		);
-		var chunkIndex = (
-			x: voxelIndex.x / width,
-			y: voxelIndex.y / depth,
-			z: voxelIndex.z / height
-		);
-
-		if ( chunkIndex.x < 0 || chunkIndex.y < 0 || chunkIndex.z < 0
-		  || chunkIndex.x >= Size.x || chunkIndex.y >= Size.y || chunkIndex.z >= Size.z ) return null;
-
-		var chunk = Chunks[chunkIndex.x, chunkIndex.y, chunkIndex.z];
-		var voxel = (
-			x: (ushort)(voxelIndex.x - chunkIndex.x * width),
-			y: (ushort)(voxelIndex.y - chunkIndex.y * depth),
-			z: (ushort)(voxelIndex.z - chunkIndex.z * height)
-		);
-
-		return new VoxelData
-		{
-			Chunk = chunk,
-			Voxel = chunk?.GetVoxel( voxel.x, voxel.y, voxel.z ),
-			Position = voxel
-		};
-	}
-
-	/// <summary>
-	/// Converts worldspace coordinates to voxel space.
-	/// </summary>
-	/// <param name="position"></param>
-	/// <returns></returns>
-	public (int x, int y, int z) GetVoxelSpace( Vector3 position )
-	{
-		var width = ChunkSize.x;
-		var depth = ChunkSize.y;
-		var height = ChunkSize.z;
-
-		var relative = position - Position;
-		var chunkPosition = new Vector3( relative.x / (width * VoxelScale), relative.y / (depth * VoxelScale), relative.z / (height * VoxelScale) );
-
-		var voxelIndex = (
-			x: (chunkPosition.x * width).FloorToInt(),
-			y: (chunkPosition.y * depth).FloorToInt(),
-			z: (chunkPosition.z * height).FloorToInt()
-		);
-
-		return voxelIndex;
-	}
-
-	[GameEvent.Client.Frame]
-	private static void destruction()
-	{
-		if ( Game.LocalPawn is not Pawn pawn )
-			return;
-
-		var ray = new Ray( pawn.Position, pawn.ViewAngles.Forward );
-		var tr = Trace.Ray( ray, 10000f )
-			.IncludeClientside()
-			.WithTag( "chunk" )
-			.Run();
-
-		var parent = (tr.Entity as ChunkEntity)?.Parent;
-		var voxelData = parent?.GetClosestVoxel( tr.EndPosition - tr.Normal * parent.VoxelScale / 2f );
-		
-		if ( voxelData?.Voxel != null )
-		{
-			var data = voxelData.Value;
-			var chunks = (IEnumerable<Chunk>)null;
-			var withPhysics = true;
-
-			if ( Input.Pressed( "attack1" ) )
-			{
-				var size = 8;
-				var result = new List<Chunk>();
-
-				for ( int x = 0; x < size; x++ )
-				for ( int y = 0; y < size; y++ )
-				for ( int z = 0; z < size; z++ )
-				{
-					var center = (Vector3)data.Position;
-					var position = center 
-						+ new Vector3( x, y, z ) 
-						- size / 2f;
-
-					if ( position.Distance( center ) >= size / 2f )
-						continue;
-
-					var res = data.Chunk.TrySetVoxel(
-						position.x.FloorToInt(),
-						position.y.FloorToInt(),
-						position.z.FloorToInt(), null );
-					result.AddRange( res.Except( result ) );
-				}
-
-				chunks = result;
-			}
-			else if ( Input.Down( "attack2" ) )
-				chunks = data.Chunk.TrySetVoxel( data.Position.x, data.Position.y, data.Position.z, new Voxel( Color32.Black ) );
-
-			if ( chunks != null )
-			{
-				foreach ( var chunk in chunks )
-					parent.GenerateChunk( chunk, withPhysics );
-			}
-		}
+		chunkEntity.SetupPhysicsFromModel( PhysicsMotionType.Static );
 	}
 
 	#region DEBUG
 	static bool debugMode = false;
-	static VoxelWorld entity;
-
-	[Event.Hotload]
-	private static void refresh()
-	{
-		if ( Game.IsServer )
-			return;
-
-		if ( entity == null )
-			return;
-
-		foreach ( var chunk in entity.Chunks )
-			entity.GenerateChunk( chunk );
-	}
-
-	[ConCmd.Client( "testvox" )]
-	public static void TestVoxel()
-	{
-		if ( entity != null )
-		{
-			foreach ( var child in entity.entities.Values )
-				child.Delete();
-
-			entity.Delete();
-		}
-
-		entity = new VoxelWorld();
-
-		var chunks = new Chunk[2, 2, 2];
-		for ( ushort x = 0; x < chunks.GetLength( 0 ); x++ )
-		for ( ushort y = 0; y < chunks.GetLength( 1 ); y++ )
-		for ( ushort z = 0; z < chunks.GetLength( 2 ); z++ )
-		{
-			var chunk = new Chunk( x, y, z, entity.ChunkSize.x, entity.ChunkSize.y, entity.ChunkSize.z, chunks );
-			chunks[x, y, z] = chunk;
-
-			for ( ushort i = 0; i < entity.ChunkSize.x; i++ )
-			for ( ushort j = 0; j < entity.ChunkSize.y; j++ )
-			for ( ushort k = 0; k < entity.ChunkSize.z; k++ )
-				chunk.SetVoxel( i, j, k, new Voxel( Color.Random.ToColor32() ) );
-		}
-
-		entity.Chunks = chunks;
-		entity.Size = new( chunks.GetLength( 0 ), chunks.GetLength( 1 ), chunks.GetLength( 2 ) );
-
-		foreach ( var chunk in chunks )
-			entity.GenerateChunk( chunk );
-	}
-
-	[ConCmd.Client( "loadvox" )]
-	public static async void LoadModel( string path )
-	{
-		if ( entity != null )
-		{
-			foreach ( var child in entity.entities.Values )
-				child.Delete();
-
-			entity.Delete();
-		}
-
-		entity = new VoxelWorld();
-
-		var chunks = await Importer.VoxImporter.Load( path, entity.ChunkSize.x, entity.ChunkSize.y, entity.ChunkSize.z );
-		entity.Size = new( chunks.GetLength( 0 ), chunks.GetLength( 1 ), chunks.GetLength( 2 ) );
-		entity.Chunks = chunks;
-
-		foreach ( var chunk in entity.Chunks )
-			entity.GenerateChunk( chunk );
-	}
 
 	[GameEvent.Client.Frame]
 	private static void debug()
@@ -402,27 +203,29 @@ public partial class VoxelWorld : ModelEntity
 			.WithTag( "chunk" )
 			.Run();
 
+		DebugOverlay.TraceResult( tr );
+
 		var parent = (tr.Entity as ChunkEntity)?.Parent;
-		var voxelData = parent?.GetClosestVoxel( tr.EndPosition - tr.Normal * parent.VoxelScale / 2f );
+		if ( parent == null )
+			return;
 
-		if ( voxelData?.Voxel != null )
-		{
-			var data = voxelData.Value;
+		var position = parent.WorldToVoxel( tr.EndPosition - tr.Normal * parent.VoxelScale / 2f );
+		var data = parent.GetByOffset( position.x, position.y, position.z );
+		if ( data.Voxel == null )
+			return;
 
-			// Debug
-			DebugOverlay.ScreenText( $"{voxelData?.Voxel?.Color ?? default}" );
-			DebugOverlay.ScreenText( $"XYZ: {voxelData?.Position}", 1 );
-			DebugOverlay.ScreenText( $"Chunk: {voxelData?.Chunk.Position}", 2 );
+		// Debug
+		DebugOverlay.ScreenText( $"{data.Voxel?.Color ?? default}" );
+		DebugOverlay.ScreenText( $"XYZ: {position}", 1 );
+		DebugOverlay.ScreenText( $"Chunk: {data.Chunk.Position}", 2 );
 
-			var voxelCenter = (Vector3)data.Chunk.Position * parent.ChunkSize * parent.VoxelScale
-				+ (Vector3)data.Position * parent.VoxelScale
-				+ parent.VoxelScale / 2f
-				+ parent.Position;
+		var voxelCenter = (Vector3)position * parent.VoxelScale
+			+ parent.VoxelScale / 2f
+			+ parent.Position;
 
-			Gizmo.Draw.Color = Color.Black;
-			Gizmo.Draw.LineThickness = 1;
-			Gizmo.Draw.LineBBox( new BBox( voxelCenter, parent.VoxelScale ) );
-		}
+		Gizmo.Draw.Color = Color.Black;
+		Gizmo.Draw.LineThickness = 1;
+		Gizmo.Draw.LineBBox( new BBox( voxelCenter, parent.VoxelScale ) );
 	}
 	#endregion
 }
