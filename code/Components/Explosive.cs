718c093e58cc1e69731b9b85123d23e218ea1130
ceitine <ceitinee@gmail.com>
2024-01-26 18:18:35 +0200

    custom explosive physics

--- code/Components/Explosive.cs
+++ code/Components/Explosive.cs
@@ -5,21 +5,26 @@ public class Explosive : Component
 	[Property] public float Duration { get; set; } = 4f;
 	[Property, Range(1, 100)] public int Radius { get; set; } = 5;
 
-	public Rigidbody Rigidbody { get; private set; }
+	public Vector3 Velocity { get; set; }
+	public GameObject GroundObject { get; set; }
+	public BoxCollider Collider { get; set; }
 
 	TimeUntil _shouldExplode;
 
 	protected override void OnAwake()
 	{
+		Collider = Components.Get<BoxCollider>( true );
 		_shouldExplode = 1;
-		Rigidbody = Components.Get<Rigidbody>();
 	}
 
 	protected override void OnUpdate()
 	{
-		Move();
-
 		Gizmo.Draw.LineSphere( new Sphere( Transform.Position, Utility.Scale * Radius ), 6 );
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		Move();
 
 		if ( _shouldExplode )
 		{
@@ -30,8 +35,58 @@ public class Explosive : Component
 
 	private void Move() // todo @ceitine: custom physics
 	{
-		var target = Rotation.LookAt( Rigidbody.Velocity );
-		Transform.Rotation = Rotation.Lerp( Transform.Rotation, target, 10f * Time.Delta );
+		// Set rotation.
+		if ( !Velocity.IsNearlyZero( 1f ) )
+			Transform.Rotation = Rotation.LookAt( Velocity.Normal );
+
+		// Apply Gravity.
+		if ( !GroundObject.IsValid() )
+			Velocity += Vector3.Down * Player.GRAVITY * Time.Delta;
+
+		// Use move helper to advance.
+		var tr = Scene.Trace
+			.Size( Collider.Scale )
+			.IgnoreGameObjectHierarchy( GameObject );
+
+		var helper = new CharacterControllerHelper( tr, Transform.Position, Velocity );
+		if ( GroundObject.IsValid() )
+			helper.Velocity *= 0.9f;
+
+		// Apply new helper values and bounce.
+		if ( helper.TryMove( Time.Delta ) > 0 )
+			Transform.Position = helper.Position;
+
+		// Let's apply some bounce.
+		var velocity = helper.Velocity;
+		var bounced = false;
+		if ( MathF.Abs( Velocity.z - velocity.z ) > 10 ) // Bounce on Z-axis.
+		{
+			velocity += Vector3.Up * -Velocity.z * 0.4f;
+			velocity *= 0.5f;
+			bounced = true;
+		}
+
+		if ( MathF.Abs( Velocity.x - velocity.x ) > 10 && !bounced ) // Bounce on X-axis.
+		{
+			velocity += Vector3.Forward * -Velocity.x * 0.4f;
+			velocity *= 0.5f;
+		}
+
+		if ( MathF.Abs( Velocity.y - velocity.y ) > 10 && !bounced ) // Bounce on Y-axis.
+		{
+			velocity += Vector3.Left * -Velocity.y * 0.4f;
+			velocity *= 0.5f;
+		}
+
+		// Finally apply final velocity.
+		Velocity = velocity;
+
+		// Check for ground collision.
+		GroundObject = Velocity.z <= 2f
+			? helper
+				.TraceFromTo( Transform.Position, Transform.Position + Vector3.Down * 2f )
+					.GameObject
+			: null;
 	}
 
 	public bool Explode()
