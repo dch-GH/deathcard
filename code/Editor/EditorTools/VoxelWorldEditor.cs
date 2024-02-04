80cb3e1c77bb3221ef6d9249ca9e7790a7ac1319
ceitine <ceitinee@gmail.com>
2024-02-04 23:39:01 +0200

    get started on tools

---
+++ code/Editor/EditorTools/VoxelWorldEditor.cs
@@ -0,0 +1,42 @@
+﻿namespace Deathcard.Editor;
+
+class VoxelWorldEditor : EditorTool<VoxelWorld>
+{
+	private static VoxelWorldEditor instance;
+	public static VoxelWorld Selected { get; set; }
+	public static bool Open
+	{
+		get => windowOpen;
+		set
+		{
+			windowOpen = value;
+			window?.Close();
+
+			if ( value )
+				instance.CreateWindow();
+		}
+	}
+
+	private static bool windowOpen;
+	private static WidgetWindow window;
+
+	public VoxelWorldEditor() : base()
+	{
+		instance = this;
+	}
+
+	void CreateWindow()
+	{
+		window = new WidgetWindow( SceneOverlay, "VoxelWorld Tools" );
+		window.MinimumWidth = 400;
+		window.MinimumHeight = SceneOverlay.Height;
+
+		window.Layout = Layout.Row();
+		AddOverlay( window, TextFlag.RightCenter, 0 );
+	}
+
+	public override void OnUpdate()
+	{
+		if ( !Selection.OfType<GameObject>().Any() ) return;
+	}
+}
